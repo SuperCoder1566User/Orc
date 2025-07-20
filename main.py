c6ece9b966a9912e94147ad59b9cab7e8c2345e5
SuperCoder1566User <aldrichanto@gmail.com>
2025-07-20 10:15:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -228,4 +228,5 @@ button { background: #4caf50; color: white; cursor: pointer; }
 
 # ---------- RUN ----------
 if __name__ == '__main__':
-    app.run(debug=True, host=0.0.0.0,port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
+
